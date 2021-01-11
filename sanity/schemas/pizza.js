7d6073c0bf6b1e1ced17d3fdcabf679ee54e7d4b
Dhadhazi <dhadhazi@gmail.com>
2021-01-11 20:30:13 +0100

    Price custom input

--- sanity/schemas/pizza.js
+++ sanity/schemas/pizza.js
@@ -1,4 +1,5 @@
 import { MdLocalPizza as icon } from 'react-icons/md';
+import PriceInput from '../components/PriceInput';
 
 export default {
   name: 'pizza',
@@ -26,6 +27,7 @@ export default {
     },
     {
       name: 'price',
+      inputComponent: PriceInput,
       title: 'Price',
       type: 'number',
       description: 'Price of the pizza in cents',
