0b227a91dfa720fc97b51196112ffb909260d21a
Dhadhazi <dhadhazi@gmail.com>
2021-01-12 14:58:12 +0100

    Graphql added

--- gatsby/gatsby-config.js
+++ gatsby/gatsby-config.js
@@ -1,1 +1,24 @@
-// This file is empty, but some people were reporting that it would not start unless they had an empty file. So here it is! You can delete the comment. Or replace it with your favourite shania twain lyrics.
+import dotenv from 'dotenv';
+
+dotenv.config({ path: '.env' });
+
+export default {
+  siteMetadata: {
+    title: 'Slicks Slices',
+    siteUrl: 'https://localhost:8000',
+    description: 'The best pizza place in Hamilton!',
+  },
+  plugins: [
+    'gatsby-plugin-styled-components',
+    {
+      // Name of the plugin
+      resolve: 'gatsby-source-sanity',
+      options: {
+        projectId: 'vw4t2omj',
+        dataset: 'production',
+        watchMode: true,
+        token: process.env.SANITY_TOKEN,
+      },
+    },
+  ],
+};
---
+++ gatsby/src/components/PizzaList.js
@@ -0,0 +1,23 @@
+import { Link } from 'gatsby';
+import React from 'react';
+
+function SinglePizza({ pizza }) {
+  return (
+    <div>
+      <Link to={`/pizza/${pizza.slug.current}`}>
+        <h2>
+          <span className="mark">{pizza.name}</span>
+        </h2>
+        <p>{pizza.toppings.map((topping) => topping.name).join(', ')}</p>
+      </Link>
+    </div>
+  );
+}
+
+export const PizzaList = ({ pizzas }) => (
+  <div>
+    {pizzas.map((pizza) => (
+      <SinglePizza pizza={pizza} key={pizza.id} />
+    ))}
+  </div>
+);
