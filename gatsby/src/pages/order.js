db67edce58c15e57326853deafd9c92fd2e4fc94
Dhadhazi <dhadhazi@gmail.com>
2021-01-14 09:51:29 +0100

    Client side form submitting

--- gatsby/src/pages/order.js
+++ gatsby/src/pages/order.js
@@ -17,10 +17,20 @@ export default function OrderPage({ data }) {
     email: '',
   });
   const pizzas = data.pizzas.nodes;
-  const { order, addToOrder, removeFromOrder } = usePizza({
+  const {
+    order,
+    addToOrder,
+    removeFromOrder,
+    error,
+    loading,
+    message,
+  } = usePizza({
     pizzas,
-    inputs: values,
+    values,
   });
+  if (message) {
+    return <p>{message}</p>;
+  }
 
   return (
     <>
@@ -87,7 +97,10 @@ export default function OrderPage({ data }) {
           <h3>
             Yout total is {formatMoney(calculateOrderTotal(order, pizzas))}
           </h3>
-          <button type="submit">Order Ahead</button>
+          <div>{error ? <p>Error: {error} </p> : ''}</div>
+          <button type="submit" disabled={loading}>
+            {loading ? 'Placing Order' : 'Order Ahead'}
+          </button>
         </fieldset>
       </OrderStyles>
     </>
